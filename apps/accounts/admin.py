6d1676f0124d27f7b6996738c2221088c0207cfc
Peeraphong Smanthap <p.smanthap@gmail.com>
2017-11-16 19:59:03 +0700

    upload admin and more git ignore

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.accounts.models import Account
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['display_name', 'last_login', 'is_superuser', 'first_name', 'last_name', 'email']
+    search_fields = ['display_name', 'username', 'first_name', 'last_name', 'email']
