67081bb0e74065525d1e0d0394e9aca0dd735232
Peeraphong Smanthap <p.smanthap@gmail.com>
2017-11-18 01:22:07 +0700

    images feature

--- Project/settings.py
+++ Project/settings.py
@@ -137,3 +137,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images/upload')
+MEDIA_URL = 'images/'
--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from apps.products.models import Product
+from apps.products.models import Product, ProductImage
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-        list_display = ('id','status','date_of_sale','seller', 'freight', 'freight_detail_choices')
+    list_display = ('id','status','date_of_sale','seller', 'freight', 'freight_detail_choices')
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('product', 'sequence', 'image_tag')
