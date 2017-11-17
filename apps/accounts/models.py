21d19dbf25d42bdc9887141a85916ba25dccb9d1
Peeraphong Smanthap <p.smanthap@gmail.com>
2017-11-18 01:35:41 +0700

    easier to create user feature

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Account(AbstractUser):
     first_name = models.CharField(verbose_name="First Name", max_length=100)
     last_name = models.CharField(verbose_name="Last Name", max_length=100)
-    email = models.EmailField(verbose_name="Email", null=True, blank=True, unique=True)
+    email = models.EmailField(verbose_name="Email", null=True, blank=True)
     display_name = models.CharField(verbose_name="Display Name", max_length=100, unique=True)
