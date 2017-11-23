47ec86fd96fab21155bc5448f40af173e5d87cec
slx2pm <lolipop_mone@hotmail.com>
2017-11-23 19:11:11 +0700

    add product and transaction

---
+++ apps/products/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from apps.products.models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        exclude = []
