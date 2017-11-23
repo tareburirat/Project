12751bd4cdc705d8126f93e7c370fa5a33537858
Burirat <tareburirat@gmail.com>
2017-11-23 20:40:48 +0700

    resolve viewsets , api_url

--- Project/api_urls.py
+++ Project/api_urls.py
@@ -1,6 +1,28 @@
 from rest_framework.routers import DefaultRouter
 
 from apps.accounts.viewsets import AccountViewSet
+from apps.addresses.viewsets import AddressViewSet
+from apps.buyers.viewsets import BuyerViewSet
+from apps.categories.viewsets import CategoryViewSet
+from apps.offers.viewsets import OfferViewSet
+from apps.orders.viewsets import OrderViewSet
+from apps.products.viewsets import ProductViewSet
+from apps.properties.viewsets import PropertyViewSet
+from apps.ratings.viewsets import RatingViewSet
+from apps.sellers.viewsets import SellerViewSet
+from apps.transactions.viewsets import TransactionViewSet
+from apps.values.viewsets import ValueViewSet
 
 router = DefaultRouter()
 router.register('accounts', AccountViewSet, base_name='account')
+router.register('addresses', AddressViewSet, base_name='address')
+router.register('buyers', BuyerViewSet, base_name='buyer')
+router.register('categories', CategoryViewSet, base_name='category')
+router.register('offers', OfferViewSet, base_name='offer')
+router.register('orders', OrderViewSet, base_name='order')
+router.register('products', ProductViewSet, base_name='product')
+router.register('properties', PropertyViewSet, base_name='property')
+router.register('ratings', RatingViewSet, base_name='rating')
+router.register('seller', SellerViewSet, base_name='seller')
+router.register('transactions', TransactionViewSet, base_name='transaction')
+router.register('values', ValueViewSet, base_name='value')
