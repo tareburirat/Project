eeb0fbfa0af3a6b82ae3a9a9221c9c25a78b686b
Burirat <tareburirat@gmail.com>
2017-12-09 14:27:36 +0700

    edit serializers seller

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,9 +1,19 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from apps.accounts.models import Account
 
+
 class AccountSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = Account
-        exclude = []
+        fields = [
+            'id',
+            'username',
+            'password',
+            'first_name',
+            'last_name',
+            'display_name',
+        ]
