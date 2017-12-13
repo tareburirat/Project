bf4a5fa207393d258aee28a429993142e09ef41c
Peeraphong Smanthap <p.smanthap@gmail.com>
2017-12-14 00:31:02 +0700

    fix controller broken

--- apps/static/js/controllers/productCtrl.js
+++ apps/static/js/controllers/productCtrl.js
@@ -1,28 +1,13 @@
-(function () {
-    'use strict';
+var app = angular.module("app", []);
+app.controller("productCtrl", function($scope, $http) {
+    $scope.products = [];
 
-    var app = angular.module("app", []);
-    app.controller("productCtrl", function($scope, MyService) {
-        $scope.product = [];
+    $http.get('http://localhost:8000/api/products/').then(function (response) {
+        $scope.products = response.data;
+        console.log($scope.products)
+    })
 
-        MyService.getProduct().then(function (data) {
-            $scope.product = data;
-        });
-
-}).factory('MyService', function ($http, $q) {
-        'use strict';
-        return {
-            getProduct : function () {
-                var deferred = $q.defer();
-
-                $http.get('http://localhost:8000/api/products/').success(function (data) {
-                        deferred.resolve(data);
-                });
-                return deferred.promise;
-            }
-        };
 }).config(function($interpolateProvider) {
   $interpolateProvider.startSymbol('[[');
   $interpolateProvider.endSymbol(']]');
 });
-})();
