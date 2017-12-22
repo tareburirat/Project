387ee45eaafd6a63a4a52879dc1fd919989dc337
Peeraphong Smanthap <p.smanthap@gmail.com>
2017-12-22 21:01:17 +0700

    backend

--- apps/products/viewsets.py
+++ apps/products/viewsets.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
@@ -7,6 +8,7 @@ from rest_framework.response import Response
 
 from apps.products.models import Product, ProductImage
 from apps.products.serializers import ProductSerializer
+from apps.properties.models import Property
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -40,3 +42,19 @@ def save_product(request):
             ProductImage.objects.create(product=product, image=image)
 
     return Response(status=status.HTTP_200_OK, data={'message': "success!"})
+
+
+@api_view(['post'])
+def get_product_properties(request):
+    try:
+        data = request.data
+        category_id = data['category_id']
+        properties = Property.objects.filter(category_id=category_id).values_list('text', flat=True)
+        if len(properties) > 1:
+            raise ValidationError
+    except KeyError:
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': 'Bad request data!!!'})
+    except ValidationError:
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': 'No properties found!!!'})
+    finally:
+        return Response(status=status.HTTP_200_OK, data={'properties': properties})
