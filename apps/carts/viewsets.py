c9d33ad28fb23bee6b0e3be4b61e86e984b0c0dd
slx2pm <lolipop_mone@hotmail.com>
2018-01-13 19:17:35 +0700

    cart

--- apps/carts/viewsets.py
+++ apps/carts/viewsets.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from apps.carts.models import Cart
@@ -7,3 +8,5 @@ from apps.carts.serializers import CartSerializer
 class CartViewSet(viewsets.ModelViewSet):
     serializer_class = CartSerializer
     queryset = Cart.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ['owner_id']
