719c6b3489f0b5920f9367456a629c56601cef9f
BENZ\Benz <benz_1996_@hotmail.com>
2018-01-17 21:35:10 +0700

    add address

--- apps/static/js/controllers/addressCtrl.js
+++ apps/static/js/controllers/addressCtrl.js
@@ -1,16 +1,21 @@
 var app = angular.module("app", []);
-    app.controller("addressCtrl", function ($scope, $http, $window) {
-        $scope.mama = 123;
+app.controller("addressCtrl", function ($scope, $window, $http) {
+    $scope.mama = 123;
 
-            $scope.getAddress = function () {
+    $scope.getBuyerId = function (buyerId) {
+        $scope.buyerId = buyerId;
+    };
+    $scope.getAddress = function () {
         var data = {
             address: $scope.address,
             sub_district: $scope.sub_district,
             district: $scope.district,
             province: $scope.province,
             zip_code: $scope.zip_code,
+            buyer: $scope.buyerId
         };
-         $http.post('http://localhost:8000/api/address_buyer/',data).then(
+        
+        $http.post('http://localhost:8000/api/addresses/', data).then(
             function (response) {
                 alert(response.data);
                 $window.location.href = '/address_buyer/';
@@ -23,7 +28,9 @@ var app = angular.module("app", []);
 
 })
 
-.config(function ($interpolateProvider) {
-   $interpolateProvider.startSymbol('[[');
-   $interpolateProvider.endSymbol(']]');
-});
+    .config(function ($interpolateProvider,$httpProvider) {
+        $interpolateProvider.startSymbol('[[');
+        $interpolateProvider.endSymbol(']]');
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    });
