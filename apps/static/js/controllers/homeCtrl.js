744e7ca98c50898cdf40311dddfc41abb07e6e66
Burirat <tareburirat@gmail.com>
2018-01-20 01:35:51 +0700

    cart

--- apps/static/js/controllers/homeCtrl.js
+++ apps/static/js/controllers/homeCtrl.js
@@ -1,10 +1,33 @@
-app.controller("homeCtrl", function ($scope, $http) {
+app.controller("homeCtrl", function ($scope, $http, $window) {
     $scope.mama = 123;
     $scope.products = [];
-    console.log(11111111111111111111);
+
+    $scope.getBuyerId = function (buyerId) {
+        $scope.buyerId = buyerId;
+    };
+
+
     $http.get('http://localhost:8000/api/products/').then(function (response) {
         $scope.products = response.data;
         console.log($scope.products);
-    })
+    });
+
+    $scope.addToCart = function () {
+        var data ={
+            owner: $scope.buyerId,
+            product: $scope.products,
+        };
+
+        $http.post('http://localhost:8000/api/carts/', data).then(
+            function (response) {
+                alert(response.data);
+                $window.location.href = '/cart/'
+            },
+            function (response) {
+                alert("failed: " + response.data)
+            }
+        )
+    }
+
 
 });
