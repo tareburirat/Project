3eea4585b0bf285cc6ff660df494f4d2917cc133
Burirat <tareburirat@gmail.com>
2018-01-20 03:33:14 +0700

    add product in cart

--- apps/static/js/controllers/productCtrl.js
+++ apps/static/js/controllers/productCtrl.js
@@ -1,7 +1,10 @@
 app.controller("productCtrl", function($scope, $http, $window) {
     $scope.products = [];
     $scope.mama = 123;
-    $scope.productId = $window.productId;
+
+    $scope.getOwnerId = function (ownerId) {
+        $scope.ownerId = ownerId;
+    };
 
     $scope.getId = function (id) {
         $scope.productId = id;
@@ -14,7 +17,23 @@ app.controller("productCtrl", function($scope, $http, $window) {
             $scope.product = response.data;
             $scope.properties = Object.keys($scope.product.property)
         })
-    }
+    };
 
+    $scope.addToCart = function () {
+        var data ={
+            owner: $scope.ownerId,
+            product: $scope.productId,
+        };
+
+        $http.post('http://localhost:8000/api/carts/', data).then(
+            function (response) {
+                // alert(response.data);
+                $window.location.href = '/cart/'
+            },
+            function (response) {
+                alert("failed: " + response.data)
+            }
+        )
+    };
 });
 
