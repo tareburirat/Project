01366183efeb43623e9fca29d04ebcc15af00fbf
slx2pm <lolipop_mone@hotmail.com>
2018-01-20 00:31:51 +0700

    input pattern, hide&show password singup login edit_profile changepassword

--- apps/static/js/controllers/inputCtrl.js
+++ apps/static/js/controllers/inputCtrl.js
@@ -1,44 +1,3 @@
-app.controller("passwordChangeCtrl", function ($scope, $http, $window) {
-    $scope.mama = 123;
-    $scope.panCardRegex = '/[A-Z]{5}\d{4}[A-Z]{1}/i';
-    $scope.getUsername = function (username) {
-        $scope.username = username;
-    };
-
-    $scope.passwordChange = function () {
-        if ($scope.newPassword !== $scope.newPasswordAgain) {
-            alert("New passwords do no match");
-            $scope.newPassword = "";
-            $scope.newPasswordAgain = "";
-            return;
-        }
-        var data = {
-            oldPassword: $scope.password,
-            newPassword1: $scope.newPassword,
-            newPassword2: $scope.newPasswordAgain
-        };
-        $http.post('http://localhost:8000/api/password_change/', data).then(
-            function (response) {
-                alert(response.data.message + " You changed your password and have to new login again.");
-
-                // var user_data = {
-                //     username: $scope.username,
-                //     password: $scope.newPassword
-                // };
-                //
-                // $http.post("http://localhost:8000/authenticate_user/", user_data).then(function (response) {
-                //     $window.location.href = '/';
-                // });
-
-                $window.location.href = '/login/';
-            },
-            function (response) {
-                alert("failed: " + response.data.message)
-            }
-        )
-    }
-
-});
 app.directive('phone', function() {
     return {
         require: 'ngModel',
@@ -56,7 +15,6 @@ app.directive('phone', function() {
         }
     };
 });
-
 app.directive('username', function() {
     return {
         require: 'ngModel',
