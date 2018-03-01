8e18712ba08acf03b22afa101b72ed1d53fb760b
slx2pm <lolipop_mone@hotmail.com>
2018-03-01 17:40:01 +0700

    edit WARNINGS: ?: (rest_framework.W001) ...

--- Project/settings.py
+++ Project/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),'home/templates/',
+            os.path.join(BASE_DIR, 'templates'), 'home/templates/',
 
         ]
         ,
@@ -114,8 +114,10 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    # # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 20
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 # Password validation
