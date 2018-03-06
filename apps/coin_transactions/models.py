96432373a4d4680465e40a194ed43ad57826232b
Peeraphong Smanthap <p.smanthap@gmail.com>
2018-03-06 19:59:04 +0700

    coin transaction take effct

--- apps/coin_transactions/models.py
+++ apps/coin_transactions/models.py
@@ -22,3 +22,12 @@ class CoinTransaction(models.Model):
     def __str__(self):
         return "{} {} {} (product_id: {})".format(self.account.display_name, self.get_transaction_type_display(),
                                                   self.amount, self.promoted_product_id)
+
+    def save(self, *args, **kwargs):
+        account = self.account
+        if self.transaction_type is self.deposit:
+            account.coin += int(self.amount)
+        elif self.transaction_type in [self.withdraw, self.usage]:
+            account.coin -= int(self.amount)
+        account.save()
+        return super(CoinTransaction, self).save(*args, **kwargs)
