71b15085cc8134b349bfab1859c18a0f249c24b3
Peeraphong Smanthap <p.smanthap@gmail.com>
2018-03-17 12:17:13 +0700

    purchased product

--- apps/admin_dashboard/urls.py
+++ apps/admin_dashboard/urls.py
@@ -5,4 +5,5 @@ from apps.admin_dashboard import views
 urlpatterns = [
     url(r'^$', views.CategoryDashBoardView.as_view()),
     url(r'^cat_summary', views.category_summary),
+    url(r'^purchased_summary', views.category_purchased_products_summary),
 ]
