80f9c19d94091f9a1c2d14e049c177795151c87b
Peeraphong Smanthap <p.smanthap@gmail.com>
2018-03-27 21:14:22 +0700

    update serializer

--- apps/bank_accounts/viewsets.py
+++ apps/bank_accounts/viewsets.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from apps.bank_accounts.models import BankAccount
@@ -7,4 +8,5 @@ from apps.bank_accounts.serializers import BankAccountSerializer
 class BankAccountViewSet(viewsets.ModelViewSet):
     serializer_class = BankAccountSerializer
     queryset = BankAccount.objects.filter(is_active=True)
-
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ['account_owner', 'id']
